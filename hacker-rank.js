8c0ea874ab331e76b878b710bdee408046ddca59
Adam Hwung <Ahwung@gmail.com>
2020-01-18 19:53:16 -0800

    finished compare triplets challenge

--- hacker-rank.js
+++ hacker-rank.js
@@ -13,3 +13,21 @@ function simpleArraySum(array) {
     return sum;
 }
 
+// Compare the Triplets
+// https://www.hackerrank.com/challenges/compare-the-triplets/problem?h_r=next-challenge&h_v=zen
+
+function compareTriplets(a, b) {
+    let score = [0, 0]
+    for (let i = 0; i < 3; i++) {
+        if (a[i] > b[i]) {
+            score[0] += 1
+        } else if (a[i] < b[i]) {
+            score[1] += 1
+        }
+    }
+    return score;
+}
+
+let first = [1,5,3];
+let second = [4,1,6]; 
+console.log(compareTriplets(first, second))
